c0beaba091082fc26fa739496d4f8e878c95efaa
Aleksei Taraskin <bakamoena@gmail.com>
2023-08-21 16:33:11 +0700

    backButton now showing only in Current Post

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {Router, RoutesRecognized} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-  constructor(private autService: AuthService) {
+
+export class NavbarComponent implements OnInit {
+
+  backBtn: boolean = false
+
+  constructor(private autService: AuthService, private route: Router) {
   }
 
+
+  ngOnInit() {
+    this.route.events.subscribe((data) => {
+      if (data instanceof RoutesRecognized) {
+        let url: string = data.state.url
+        this.backBtn = url !== "/posts";
+      }
+    });
+  }
 
   logOut() {
     this.autService.logout()
